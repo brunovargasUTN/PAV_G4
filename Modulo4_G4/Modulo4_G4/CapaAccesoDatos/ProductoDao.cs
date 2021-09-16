0fcaeb473572136f540f904db2eb09e85f114d37
Ivan Gomez <ivangomez854@gmail.com>
2021-09-16 19:28:45 -0300

    Se realiza capa dao de Producto, Proyecto

--- Modulo4_G4/Modulo4_G4/CapaAccesoDatos/ProductoDao.cs
+++ Modulo4_G4/Modulo4_G4/CapaAccesoDatos/ProductoDao.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Modulo4_G4.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Modulo4_G4.CapaAccesoDatos
 {
     class ProductoDao
     {
+        public IList<Producto> GetAll()
+        {
+            List<Producto> listadoBugs = new List<Producto>();
+
+            var strSql = "SELECT id_producto, nombre from Productos";
+
+            var resultadoConsulta = DataManager.GetInstance().ConsultaSQL(strSql);
+
+            foreach (DataRow row in resultadoConsulta.Rows)
+            {
+                listadoBugs.Add(ObjectMapping(row));
+            }
+
+            return listadoBugs;
+        }
+
+        private Producto ObjectMapping(DataRow row)
+        {
+            Producto oProducto = new Producto
+            {
+                IdProducto = Convert.ToInt32(row["id_producto"].ToString()),
+                NombreProducto = row["nombre"].ToString()
+            };
+
+            return oProducto;
+        }
     }
 }
