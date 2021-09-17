72eb9b4f3b9bb57b2cedaef5091bdaa44b3ab1dc
JAVIER RICSE <javierricse@gmail.com>
2021-09-16 22:55:40 -0300

    ahora si we :'v

--- Modulo4_G4/Modulo4_G4/CapaAccesoDatos/BarrioDao.cs
+++ Modulo4_G4/Modulo4_G4/CapaAccesoDatos/BarrioDao.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using Modulo4_G4.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Modulo4_G4.CapaAccesoDatos
 {
-    class BarrioDao
+    public class BarrioDao
     {
+        public IList<Barrio> GetAll()
+        {
+            List<Barrio> list = new List<Barrio>();
+            var strSql = "SELECT b.id_barrio, b.nombre FROM Barrios b";
+            var resultConsulta = DataManager.GetInstance().ConsultaSQL(strSql);
+            foreach (DataRow row in resultConsulta.Rows)
+            {
+                list.Add(ObjectMapping(row));
+            }
+            return list;
+        }
+
+        public Barrio GetBarrioById(int idBarrio)
+        {
+            var strSql = String.Concat("SELECT b.id_barrio, ",
+                           "       b.nombre,",
+                           "FROM Barrios b,",
+                           "WHERE b.id_barrio = " + idBarrio.ToString());
+            return ObjectMapping(DataManager.GetInstance().ConsultaSQL(strSql).Rows[0]);
+
+        }
+
+        private Barrio ObjectMapping(DataRow row)
+        {
+            Barrio oBarrio = new Barrio
+            {
+                IdBarrio = Convert.ToInt32(row["id_barrio"].ToString()),
+                NombreBarrio = row["nombre"].ToString()
+            };
+
+            return oBarrio;
+        }
     }
 }
--- Modulo4_G4/Modulo4_G4/CapaLogicaDeNegocio/BarrioService.cs
+++ Modulo4_G4/Modulo4_G4/CapaLogicaDeNegocio/BarrioService.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Modulo4_G4.CapaAccesoDatos;
+using Modulo4_G4.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Modulo4_G4.CapaLogicaDeNegocio
 {
-    class BarrioService
+    public class BarrioService
     {
+        private BarrioDao oBarrioDao;
+        public BarrioService()
+        {
+            oBarrioDao = new BarrioDao();
+        }
+
+        public IList<Barrio> ObtenerTodos()
+        {
+            return oBarrioDao.GetAll();
+        }
+
+        public Barrio ConsultarBarrioPorId(int id)
+        {
+            return oBarrioDao.GetBarrioById(id);
+        }
     }
 }
