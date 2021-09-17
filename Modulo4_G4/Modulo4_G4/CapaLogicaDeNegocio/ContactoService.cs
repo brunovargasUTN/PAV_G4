83634298b5fe1563dce287f1f02670c55412485f
JAVIER RICSE <javierricse@gmail.com>
2021-09-16 22:28:17 -0300

    contacto | usuario

--- Modulo4_G4/Modulo4_G4/CapaLogicaDeNegocio/ContactoService.cs
+++ Modulo4_G4/Modulo4_G4/CapaLogicaDeNegocio/ContactoService.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Modulo4_G4.Entidades;
+using Modulo4_G4.CapaAccesoDatos;
 
 namespace Modulo4_G4.CapaLogicaDeNegocio
 {
-    class ContactoService
+    public class ContactoService
     {
+        private ContactoDao oContactoDao;
+        public ContactoService()
+        {
+            oContactoDao = new ContactoDao();
+        }
+        public IList<Contacto> ObtenerTodos()
+        {
+            return oContactoDao.GetAll();
+        }
     }
 }
