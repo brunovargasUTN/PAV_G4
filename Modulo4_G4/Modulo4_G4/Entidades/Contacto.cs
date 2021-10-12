74d21ab49d3ef083f119d450d63470505c8132ea
JAVIER RICSE <javierricse@gmail.com>
2021-10-12 18:19:04 -0300

    cambios

--- Modulo4_G4/Modulo4_G4/Entidades/Contacto.cs
+++ Modulo4_G4/Modulo4_G4/Entidades/Contacto.cs
@@ -10,7 +10,7 @@ namespace Modulo4_G4.Entidades
         public string NombreContacto { get; set; }
         public string Apellido { get; set; }
         public string EmailContacto { get; set; }
-        public int Telefono { get; set; }
+        public string Telefono { get; set; }
 
         public override string ToString()
         {
